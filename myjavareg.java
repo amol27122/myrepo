a67391a63f0dc92adfa35acd84dd67968791f296
amol <amolbadode95@gmail.com>
2022-04-15 12:52:28 +0530

    my task complited

--- myjavareg.java
+++ myjavareg.java
@@ -1,3 +1,7 @@
 this is my registration page.
 this is my magager work complited.
-
+1st day of java lines of code.
+2nd day of java lines of code.
+3rd day of java lines of code.
+4th day of java lines of code.
+5th day of java lines of code.
